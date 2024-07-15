c4c15e42b5a7421f13a89a631b7c45659ffa1d17
maureen <maureen.murimi@moringaschool.com>
2024-07-16 00:44:48 +0300

    Modified flashcards by adding try/except

--- Flash-card-App/main.py
+++ Flash-card-App/main.py
@@ -6,22 +6,43 @@ BACKGROUND_COLOR = "#B1DDC6"
 LANGAUAGE = ("Ariel", 40, "italic")
 MYWORD = ("Ariel", 60, "bold")
 timer = None
+current_word = {}
+words = {}
+
 # ---------------------------- GENERATE WORD ------------------------------- #
 # data from csv
-data = pandas.read_csv("data/french_words.csv")
-words = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except:
+    original_data = pandas.read_csv("data/french_words.csv")
+    original_data.to_dict(orient="records")
+else:
+    words = data.to_dict(orient="records")
+
 
 def generate_word():
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
     current_word = choice(words)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_word["French"])
-    
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_word["French"], fill="black")
+    # To change it back the image
+    canvas.itemconfig(canvas_image, image = card_front)
+    flip_timer = window.after(3000, func=flip_card)
+
 # ---------------------------- FLIP CARD ------------------------------- #
-
-
 def flip_card():
-    window.after(3000 )
-    
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_word["English"], fill="white" )
+    # To change the image
+    canvas.itemconfig(canvas_image, image = card_back)
+# ---------------------------- SAVE PROGRESS ------------------------------- #
+
+def correct_answer():
+    words.remove(current_word)
+    words_to_learn =pandas.DataFrame(words)
+    words_to_learn.to_csv("words_to_learn.csv", index=False)
+    generate_word()
     
 # ---------------------------- UI SETUP ------------------------------- #
 # window
@@ -29,6 +50,7 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
 # canvas
 canvas = Canvas(width = 800, height = 526,bg= BACKGROUND_COLOR,  highlightthickness=0 )
 # front
@@ -39,16 +61,13 @@ canvas.grid(column=0, row=0, columnspan=2)
 # back
 card_back = PhotoImage(file="images/card_back.png")
 
-# To change the image
-canvas.itemconfig(canvas_image, image = card_back)
-
 # text
 card_title= canvas.create_text(400, 150, text="Title", font=LANGAUAGE)
 card_word = canvas.create_text(400, 263, text=f"Word", font= MYWORD)
 
 # Buttons
 right = PhotoImage(file="images/right.png")
-right_button = Button(image=right, highlightthickness=0, command=generate_word)
+right_button = Button(image=right, highlightthickness=0, command=correct_answer)
 right_button.grid(column=1, row=1)
 
 wrong = PhotoImage(file="images/wrong.png")
@@ -56,8 +75,11 @@ wrong_button = Button(image=wrong, highlightthickness=0, command=generate_word)
 wrong_button.grid(column=0, row=1)
 
 
-# create txt file for words
-
+    
+    
+    
+    # create csv file for words
+    
 # call our generate card
 generate_word()
 
